a1d0817ac8e10fc3bf16aa9133a75c8f2375df22
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-10-13 17:31:53 -0200

    Update mansao.lua

--- assets/preload/stages/mansao.lua
+++ assets/preload/stages/mansao.lua
@@ -59,19 +59,7 @@ function onUpdate()
 				triggerEvent('Camera Follow Pos',xx,yy)
 			end
 		else
-			if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
-				triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
-			elseif getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
-				triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
-			elseif getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
-				triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
-			elseif getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
-				triggerEvent('Camera Follow Pos',xx2,yy2+10)
-			elseif getProperty('boyfriend.animation.curAnim.name') == 'idle' then
-				triggerEvent('Camera Follow Pos',xx2,yy2)
-			end
-		end
-	else
+
 		triggerEvent('Camera Follow Pos','','')
 	end
 	if lowQuality then
